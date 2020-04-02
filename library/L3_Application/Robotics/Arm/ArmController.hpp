9906b29349a85916bca3658fbc8999138c6bf8e8
naterpotatoers <nate.garza@hotmail.com>
2020-04-01 18:11:17 -0700

    initial documentation

--- library/L3_Application/Robotics/Arm/ArmController.hpp
+++ library/L3_Application/Robotics/Arm/ArmController.hpp
@@ -1,3 +1,10 @@
+/**
+ * Header file for arm control functionality.
+ *
+ * @author SJSU Control Systems
+ * @version Spring 2020
+ */
+
 #pragma once
 
 #include "utility/units.hpp"
@@ -11,6 +18,16 @@ namespace robotics
 class ArmController
 {
  public:
+  /**
+   * Contructs armController object. Takes in 3 servo objects as parameters.
+   *
+   * @param shoulder The ChiHaiServo object which controls the motor outside the
+   * wrist.
+   * @param wrist The ChiHaiServo object which controls the motor located within
+   * the wrist.
+   * @param rotunda The RotundaServo object which controls entire arm direction.
+   * Located at arm base.
+   */
   explicit constexpr ArmController(sjsu::robotics::ChiHaiServo & shoulder,
                                    sjsu::robotics::ChiHaiServo & wrist,
                                    sjsu::robotics::RotundaServo & rotunda)
@@ -18,6 +35,9 @@ class ArmController
   {
   }
 
+  /**
+   * Begins shoulder, wrist, and rotunda motor setup.
+   */
   void Initialize()
   {
     shoulder_.Initialize();
@@ -25,22 +45,41 @@ class ArmController
     rotunda_.Initialize();
   }
 
+  /**
+   * Sets shoulder servo motor to desired angle.
+   *
+   * @param angle the number of degrees to move shoulder.
+   */
   void SetShoulderAngle(units::angle::degree_t angle)
   {
     shoulder_.SetAngle(angle);
   }
 
+  /**
+   * Sets wrist servo motor to desired angle.
+   *
+   * @param angle the number of degrees to move wrist.
+   */
   void SetWristAngle(units::angle::degree_t angle)
   {
     wrist_.SetAngle(angle);
   }
 
+  /**
+   * Sets rotunda servo motor to desired angle.
+   *
+   * @param angle the number of degrees to move rotunda.
+   */
   void SetRotundaAngle(units::angle::degree_t angle)
   {
     rotunda_.SetAngle(angle);
   }
 
-  void Stop() {
+  /**
+   * Stops the shoulder and wrist servo motors.
+   */
+  void Stop()
+  {
     shoulder_.Stop();
     wrist_.Stop();
   }
--- library/L3_Application/Robotics/Arm/RotundaServo.hpp
+++ library/L3_Application/Robotics/Arm/RotundaServo.hpp
@@ -1,3 +1,10 @@
+/**
+ * Header file for rotunda servo motor functionality.
+ *
+ * @author SJSU Control Systems
+ * @version Spring 2020
+ */
+
 #pragma once
 #include "L2_HAL/actuators/servo/servo.hpp"
 
@@ -8,8 +15,18 @@ namespace robotics
 class RotundaServo : public Servo
 {
  public:
+  /**
+   * Contructs RotundaServo object. Takes in 1 pwm pin parameter.
+   *
+   * @param pwm The pin to control the RotundaServo object.
+   */
   explicit constexpr RotundaServo(const Pwm & pwm) : Servo(pwm) {}
 
+  /**
+   * Establishes frequency, pulse, and angle bounds.
+   * 
+   * @param frequency Defaulted to 50_Hz for servo motors. 
+   */
   void Initialize(units::frequency::hertz_t frequency = 50_Hz) override
   {
     Servo::Initialize(frequency);
